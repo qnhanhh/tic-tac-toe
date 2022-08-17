21cdd197898a8b959bcc5619f8e43c5b0398e5b3
qnhanhh <caoquynhanh18@gmail.com>
2022-08-17 15:14:52 +0700

    add undo and redo

--- src/index.js
+++ src/index.js
@@ -15,38 +15,18 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-    //all react component classes that have a constructor should start with a super(props) call
-    constructor(props) {
-        super(props) //super is needed when defining the constructor of a subclass
-        this.state = {
-            squares: Array(9).fill(null),
-            xIsNext: true
-        }
-    }
-
     renderSquare(i) {
         return <Square
-            value={this.state.squares[i]}
-            onClick={() => this.handleClick(i)}
+            value={this.props.squares[i]}
+            onClick={() => this.props.onClick(i)}
         />;
     }
 
-    handleClick(i) {
-        //use slice to create a copy of the squares array instead of modifying the existing array
-        const squares = this.state.squares.slice()
-        squares[i]=this.state.xIsNext?'X':'O'
-        this.setState({
-            squares:squares,
-            xIsNext: !this.state.xIsNext
-        })
-    }
+
 
     render() {
-        const status = `Next player: ${this.state.xIsNext?'X':'O'}`;
-
         return (
             <div>
-                <div className="status">{status}</div>
                 <div className="board-row">
                     {this.renderSquare(0)}
                     {this.renderSquare(1)}
@@ -68,22 +48,100 @@ class Board extends React.Component {
 }
 
 class Game extends React.Component {
+    //all react component classes that have a constructor should start with a super(props) call
+    constructor(props) {
+        super(props) //super is needed when defining the constructor of a subclass
+        this.state = {
+            history: [{
+                squares: Array(9).fill(null)
+            }],
+            stepNumber: 0,
+            xIsNext: true
+        }
+    }
     render() {
+        const history = this.state.history
+        const current = history[this.state.stepNumber]
+        const winner = calculateWinner(current.squares)
+
+        const moves = history.map((step, move) => {
+            const desc = move ? `Go to move #${move}` : 'Go to game start'
+            return (
+                <li>
+                    <button key={move} onClick={() => { this.jumpTo(move) }}>{desc}</button>
+                </li>
+            )
+        })
+
+        let status
+        if (winner) {
+            status = `Winner: ${winner}`
+        } else {
+            status = `Next player: ${this.state.xIsNext ? 'X' : 'O'}`
+        }
+
         return (
             <div className="game">
                 <div className="game-board">
-                    <Board />
+                    <Board
+                        squares={current.squares}
+                        onClick={(i) => this.handleClick(i)}
+                    />
                 </div>
                 <div className="game-info">
-                    <div>{/* status */}</div>
-                    <ol>{/* TODO */}</ol>
+                    <div>{status}</div>
+                    <ol>{moves}</ol>
                 </div>
             </div>
         );
     }
+
+    handleClick(i) {
+        const history = this.state.history.slice(0, this.state.stepNumber+1)
+        const current = history[history.length - 1]
+        //use slice to create a copy of the squares array instead of modifying the existing array
+        const squares = current.squares.slice()
+        if (calculateWinner(squares) || squares[i]) {
+            return;
+        }
+        squares[i] = this.state.xIsNext ? 'X' : 'O'
+        this.setState({
+            history: history.concat([{
+                squares: squares
+            }]),
+            stepNumber: history.length,
+            xIsNext: !this.state.xIsNext
+        })
+    }
+
+    jumpTo(move){
+        this.setState({
+            stepNumber:move,
+            xIsNext: (move%2)===0
+        })
+    }
 }
 
 // ========================================
+function calculateWinner(squares) {
+    const lines = [
+        [0, 1, 2],
+        [3, 4, 5],
+        [6, 7, 8],
+        [0, 3, 6],
+        [1, 4, 7],
+        [2, 5, 8],
+        [0, 4, 8],
+        [2, 4, 6],
+    ]
 
+    for (let i = 0; i < lines.length; i++) {
+        const [a, b, c] = lines[i]
+        if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+            return squares[a]
+        }
+    }
+    return null;
+}
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Game />);
